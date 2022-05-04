e4f1f15258422612b16ca6ec87b24fe87409947e
Andreas Owe Aabø <andreasoweaabo@gmail.com>
2022-05-04 17:08:11 +0200

    Set up and Todo

---
+++ src/main/java/com/endand/todoplatform/TodoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.endand.todoplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endand/todoplatform/TodoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endand.todoplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
