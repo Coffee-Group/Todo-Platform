343314274b94221d5bb455098d46ef5fcffa720e
Endre Tønnessen <Endre.Tonnessen@student.uib.no>
2022-05-06 17:33:24 +0200

    Setup

--- src/main/java/com/endand/todoplatform/TodoPlatformApplication.java
+++ src/main/java/com/endand/todoplatform/TodoPlatformApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoPlatformApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Test");
 		SpringApplication.run(TodoPlatformApplication.class, args);
 	}
+
+
 
 }
